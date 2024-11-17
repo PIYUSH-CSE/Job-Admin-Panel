00d5eed85c4b75a044ff0b742b04b5b374fb3f63
Piyush Gupta <111417820+PIYUSH-CSE@users.noreply.github.com>
2024-11-17 13:24:09 +0530

    Update settings.py

--- indeeds/settings.py
+++ indeeds/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = '39+0j4utwu%5e2=_o#t8tr1!e#-)ba9c(zw_ft&x(xr@^nhfiv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'job-admin-panel-production.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://job-admin-panel-production.up.railway.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
